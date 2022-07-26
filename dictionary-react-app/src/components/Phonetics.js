6be924692e45c821eadb91f0145bea731571825f
Edward Wang <ewang43@jhu.edu>
2022-07-25 22:45:44 -0400

    Finish the Phonetics section

--- dictionary-react-app/src/components/Phonetics.js
+++ dictionary-react-app/src/components/Phonetics.js
@@ -1,5 +1,39 @@
 function Phonetics() {
-    return <div>Phonetics!</div>;
+
+    const phonetics = [
+        {
+            text: "/həˈloʊ/",
+            audio: "https://lex-audio.useremarkable.com/mp3/hello_us_1_rr.mp3",
+        },
+        {
+            text: "/hɛˈloʊ/",
+            audio: "https://lex-audio.useremarkable.com/mp3/hello_us_2_rr.mp3",
+        }
+    ];
+    return (
+        <>
+            <section className="section is-medium pt-0 pb-6" id="phonetics">
+                <h1 className="title">Phonetics</h1>
+                {
+                    phonetics.map((phonetic, index) => {
+                        return (
+                            <article className="message is-medium" key={index}>
+                                <div className = "message-header">
+                                    {phonetic.text}
+                                </div>
+                                <div className = "message-body">
+                                    <audio controls style={{width: "100%"}}>
+                                        <source src={phonetic.audio} type = "audio/mpeg"/>
+                                        Your browser doesn't support the audio element.
+                                    </audio>
+                                </div>
+                            </article>
+                        )
+                    })
+                }
+            </section>
+        </>
+    )
 }
 
 export default Phonetics;
