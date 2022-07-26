8bc8f850af97affde9a3bde7fe7f45c948b4eddd
Edward Wang <ewang43@jhu.edu>
2022-07-25 22:57:38 -0400

    Finish definitions

--- dictionary-react-app/src/components/Definitions.js
+++ dictionary-react-app/src/components/Definitions.js
@@ -50,7 +50,7 @@ function Definitions() {
                                 </div>
                             </div>
                         </article>
-                    )
+                    );
                 })
             }
         </section>
