299ccb3998f41fada7ff4f57b0e61c7a413e947f
Edward Wang <ewang43@jhu.edu>
2022-07-26 18:36:18 -0400

    Modify search to utilize states

--- dictionary-react-app/src/components/Search.js
+++ dictionary-react-app/src/components/Search.js
@@ -1,5 +1,30 @@
-function Search() {
-    return <div>Search!</div>;
+import React, { Component } from "react";
+
+class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            word: "hello"
+        }
+    }
+
+    changeWord = (event) => {
+        this.setState({word: event.target.value});
+    }
+    render() {
+        return (
+            <section className="section">
+                <div className="field has-addons">
+                    <div className="control is-expanded">
+                        <input className="input is-large is-fullwide" id="define-input" placeholder="Enter a Word" type="text" value={this.state.word} onChange={this.changeWord} />
+                    </div>
+                    <div className="control">
+                        <button className="button is-info is-large" id="define-btn">Define</button>
+                    </div>
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Search;
